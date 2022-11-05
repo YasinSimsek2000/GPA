2ccffa6bfe5e125a4e629e382375aa4ae6d4475e
YasinSimsek2000 <simsekyasin2900@gmail.com>
2022-11-06 01:09:56 +0300

    First

---
+++ src/main/java/com/example/gpa/GpaApplication.java
@@ -0,0 +1,11 @@
+package com.example.gpa;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GpaApplication.class, args);
+    }
+
+}
